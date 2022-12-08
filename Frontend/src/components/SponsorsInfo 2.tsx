c259d49840c0462d3bbd21014e37adf2f8fe3dca
gislainm <gmuhikira2@gmail.com>
2022-12-07 18:31:08 -0600

    addition changes ready for deployment

--- Frontend/src/components/SponsorsInfo 2.tsx
+++
@@ -1,9 +0,0 @@
-import SponsorDets from "./Sponsor"
-
-export default function Sponsor({ sponsor }: any): JSX.Element {
-    return (
-        <div className="flex items-center justify-center flex-col">
-            <SponsorDets name={sponsor.name} link={sponsor.link} />
-        </div>
-    )
-}
